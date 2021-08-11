fc087fd2fd3391c8dfba5cee32e0c818cfec86bb
Cjhightower38 <Cjhightower38@gmail.com>
2021-08-11 19:47:56 -0400

    add better documentation

--- highs_lows.py
+++ highs_lows.py
@@ -8,6 +8,8 @@ with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 	
+	# Removed print(header_row) in exchange of print(ind, col_header)
+	# for more detail. enumerate gives the index of each item.
 	for index, column_header in enumerate(header_row):
 		print(index, column_header)
 	
