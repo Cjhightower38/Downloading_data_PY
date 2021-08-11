9988091f7bcac8bad8e48e65e6c6a93c3aa65269
Cjhightower38 <Cjhightower38@gmail.com>
2021-08-11 18:59:41 -0400

    add headers and positions

--- highs_lows.py
+++ highs_lows.py
@@ -7,5 +7,7 @@ filename = 'sitka_weather_2018_simple.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
-	print(header_row)
+	
+	for index, column_header in enumerate(header_row):
+		print(index, column_header)
 	
