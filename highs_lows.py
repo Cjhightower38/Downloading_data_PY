e998f17346200ebb8e7b175c496142492c4e219a
Cjhightower38 <Cjhightower38@gmail.com>
2021-08-12 17:48:40 -0400

    add temperature chart using matplotlib

--- highs_lows.py
+++ highs_lows.py
@@ -1,4 +1,6 @@
-# Import csv module
+# Import csv module and matplotlib module
+from matplotlib import pyplot as plt
+
 import csv
 
 # Store the download file in a variable, open the variable as f read it
@@ -19,3 +21,16 @@ with open(filename) as f:
 		highs.append(high)
 		
 	print(highs)
+	
+# Using pyplot to plot the data
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# Formatting plot to include title, x and y lables, and tick parameters.
+plt.title('Daily high temperatures for the year 2018', fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature(F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
